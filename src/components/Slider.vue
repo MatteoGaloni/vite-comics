f91a271efb7ac62c21c053b1a14f2434ff8a72ca
MatteoGaloni <mtgaloni@gmail.com>
2023-05-22 15:04:03 +0200

    add slides layout

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -6,8 +6,19 @@ export default {
 
 <template>
         <div id="slider_wrapper">  
-            <div class="container p-2">        
-                <h5>CONTENT GOES HERE</h5>
+            <div class="container p-2 d-flex flex-wrap">        
+                <div class="slide">1</div>
+                <div class="slide">2</div>
+                <div class="slide">3</div>
+                <div class="slide">4</div>
+                <div class="slide">5</div>
+                <div class="slide">6</div>
+                <div class="slide">1</div>
+                <div class="slide">2</div>
+                <div class="slide">3</div>
+                <div class="slide">4</div>
+                <div class="slide">5</div>
+                <div class="slide">6</div>
             </div>           
         </div> 
 </template>
@@ -17,6 +28,12 @@ export default {
     color: white;
     background-color: rgb(28, 28, 28);
     margin: auto;
+}
+
+
+
+.slide{
+    width: calc(100% / 6);
 }
 
 </style>
